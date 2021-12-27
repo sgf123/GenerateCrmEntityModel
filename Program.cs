27f0a8955bd1e0a618269d6189b1d02c94cefdcd
sgf123 <1109602531@qq.com>
2021-12-27 17:09:51 +0800

    v11

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace GenerateCrmEntityModel
     {
         static void Main(string[] args)
         {
-            //1.2.3.5.6.7.8.9
+            //1.2.3.5.6.7.8.9.a
             Generate("entityname", "modelname");
             Console.WriteLine("success!");
             Console.ReadLine();
